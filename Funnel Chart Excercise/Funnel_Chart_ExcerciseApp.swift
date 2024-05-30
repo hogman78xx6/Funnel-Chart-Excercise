63a777b43453c32452897720dfa4d17c50d981a3
Michael Knych <mmknych@yahoo.com>
2024-05-30 17:18:41 -0500

    Initial Commit

---
+++ Funnel Chart Excercise/Funnel_Chart_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Funnel_Chart_ExcerciseApp.swift
+//  Funnel Chart Excercise
+//
+//  Created by Michael Knych on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Funnel_Chart_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
