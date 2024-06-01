78884e1fe8569b2d0d02f7db9cb3a6719dc18fec
Michael Knych <mmknych@yahoo.com>
2024-06-01 12:12:00 -0500

    First commit after v1 of app

---
+++ Funnel Chart Excercise/ChartData.swift
@@ -0,0 +1,8 @@
+//
+//  ChartData.swift
+//  Funnel Chart Excercise
+//
+//  Created by Michael Knych on 5/30/24.
+//
+
+import Foundation
---
+++ Funnel Chart Excercise/ExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleView.swift
+//  Funnel Chart Excercise
+//
+//  Created by Michael Knych on 5/30/24.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleView()
+}
