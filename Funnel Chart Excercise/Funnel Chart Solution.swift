45a1aac9a2faad6794223a2118daf0c08024cde3
Michael Knych <mmknych@yahoo.com>
2024-06-01 18:57:00 -0500

    Addition of Solution view recreate from video solution

---
+++ Funnel Chart Excercise/Funnel Chart Solution.swift
@@ -0,0 +1,18 @@
+//
+//  Funnel Chart Solution.swift
+//  Funnel Chart Excercise
+//
+//  Created by Michael Knych on 6/1/24.
+//
+
+import SwiftUI
+
+struct Funnel_Chart_Solution: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Funnel_Chart_Solution()
+}
